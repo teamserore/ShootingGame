9962d0bea5c0db457b5189fb41d64989108892ae
boo <boo>
2016-07-03 16:15:53 +0900

    [SHBoo] 오디오버그수정, 나이프 수정중

--- Assets/Scripts/BgmManager.cs
+++ Assets/Scripts/BgmManager.cs
@@ -17,37 +17,46 @@ public class BgmManager : MonoBehaviour {
     AudioSource audio;
     // Bgm
     private AudioClip mainBgm = null;
-    private AudioClip stageBgm1 = null;
-    private AudioClip stageBgm2 = null;
-    private AudioClip stageBgm3 = null;
+    private AudioClip[] stageBgm = null;
 
     void Start () {
-        audio = GetComponent<AudioSource>();
+        
         DontDestroyOnLoad(gameObject);
         mainBgm = (AudioClip)Resources.Load("Sounds/Bgm/MainBgm") as AudioClip;
-        stageBgm1 = (AudioClip)Resources.Load("Sounds/Bgm/Stage1") as AudioClip;
-        stageBgm2 = (AudioClip)Resources.Load("Sounds/Bgm/Stage2") as AudioClip;
-        stageBgm3 = (AudioClip)Resources.Load("Sounds/Bgm/Stage3") as AudioClip;
+        stageBgm = new AudioClip[3];
+        //stageBgm1 = (AudioClip)Resources.Load("Sounds/Bgm/Stage1") as AudioClip;
+        //stageBgm2 = (AudioClip)Resources.Load("Sounds/Bgm/Stage2") as AudioClip;
+        //stageBgm3 = (AudioClip)Resources.Load("Sounds/Bgm/Stage3") as AudioClip;
     }
 
     public void PlayMainBgm() {
+        if(audio == null) {
+            audio = GetComponent<AudioSource>();
+        }
         audio.clip = mainBgm;
         audio.Play();
     }
 
     public void PlayGameBgm(int stage) {
-        switch (stage) {
-            case 1:
-                audio.clip = stageBgm1;
-                break;
-            case 2:
-                audio.clip = stageBgm2;
-                break;
-            case 3:
-                audio.clip = stageBgm3;
-                break;
+        if (audio == null) {
+            audio = GetComponent<AudioSource>();
         }
+        if(stageBgm[stage] == null) {
+            stageBgm[stage] = (AudioClip)Resources.Load("Sounds/stage" + stage) as AudioClip;
+        }
+        audio.clip = stageBgm[stage];
         audio.Play();
+        //switch (stage) {
+        //    case 1:
+        //        audio.clip = stageBgm1;
+        //        break;
+        //    case 2:
+        //        audio.clip = stageBgm2;
+        //        break;
+        //    case 3:
+        //        audio.clip = stageBgm3;
+        //        break;
+        //}
     }
 
     public void BgmValueChanged(float value) {
--- Assets/Scripts/Game/Enemy/KniefEnemy.cs
+++ Assets/Scripts/Game/Enemy/KniefEnemy.cs
@@ -2,27 +2,29 @@
 using System.Collections;
 
 public class KniefEnemy : EnemyScript {
-    private Transform playerTr = null;
+    private Vector2 playerPos;
     private SpriteRenderer kniefSprite = null;
-    public GameObject kniefWarningSign = null;
-    public GameObject kniefWarningLine = null;
+    private GameObject kniefWarningSign = null;
+    private GameObject kniefWarningLine = null;
 
     void Awake() {
         EnemyIO.getInstance.GetEnemyData(EnemyType.KniefEnemy, out enemyInfo);
         kniefSprite = this.gameObject.GetComponent<SpriteRenderer>();
-        playerTr = GameObject.FindWithTag("Player").GetComponent<Transform>();
-        //경고선 처리
-        StartCoroutine(KniefWarningLine());
-        //경고느낌표 처리
-        StartCoroutine(KniefWarningSign());
+        playerPos = GameObject.FindWithTag("Player").GetComponent<Transform>().position;
+        kniefWarningLine = GameObject.Find("KniefWarningLine");
+        kniefWarningSign = GameObject.Find("KniefWarningSign");
+        kniefWarningLine.SetActive(false);
+        kniefWarningSign.SetActive(false);
     }
     IEnumerator Start() {
         //밑으로 진행
-        //kniefSprite.enabled = false; //이미지 안보이게 
-        yield return new WaitForSeconds(9.0f);
-        //kniefSprite.enabled = true;
-        //this.transform.position = new Vector2(playerTr.position.x, 9.0f);
-        //this.transform.Translate(Vector2.down * Time.deltaTime * enemyInfo.speed); // 아래로 진행
+        kniefSprite.enabled = false;
+        StartCoroutine(KniefWarningLine()); //경고선 처리
+        StartCoroutine(KniefWarningSign()); //경고느낌표 처리
+        yield return new WaitForSeconds(1.1f);
+        kniefSprite.enabled = true;
+        this.transform.position = new Vector2(playerPos.x, 9.0f);
+        this.transform.Translate(Vector2.down * Time.deltaTime * enemyInfo.speed); // 아래로 진행
     }
     void Update() {
         this.transform.Rotate(0.0f, 0.0f, Time.deltaTime * 150); // 회전
@@ -37,17 +39,15 @@ public class KniefEnemy : EnemyScript {
         }
     }
     //나이프경고라인 처리 코루틴
-    private IEnumerator KniefWarningLine() {
+    private IEnumerator KniefWarning() {
         kniefWarningLine.SetActive(true);
-        kniefWarningLine.transform.position = new Vector2(playerTr.position.x, 0);
-        yield return new WaitForSeconds(4.0f);
+        kniefWarningLine.transform.position = new Vector2(playerPos.x, 0);
+        yield return new WaitForSeconds(2.0f);
         kniefWarningLine.SetActive(false);
-    }
     //나이프경고사인 처리 코루틴
-    private IEnumerator KniefWarningSign() {
         kniefWarningSign.SetActive(true);
-        kniefWarningSign.transform.position = new Vector2(playerTr.position.x, 9.0f);
-        yield return new WaitForSeconds(5.0f);
+        kniefWarningSign.transform.position = new Vector2(playerPos.x, 9.0f);
+        yield return new WaitForSeconds(2.0f);
         kniefWarningSign.SetActive(false);
     }
 }
