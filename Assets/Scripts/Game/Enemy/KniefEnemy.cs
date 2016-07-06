02948d1c13ea50944595baf3f7e2436995adc910
boo <boo>
2016-07-06 21:51:32 +0900

    [SHBoo]에러수정 제발

--- Assets/Scripts/Game/Enemy/KniefEnemy.cs
+++ Assets/Scripts/Game/Enemy/KniefEnemy.cs
@@ -4,30 +4,26 @@ using System.Collections;
 public class KniefEnemy : EnemyScript {
     private Vector2 playerPos;
     private SpriteRenderer kniefSprite = null;
-    private GameObject kniefWarningSign = null;
-    private GameObject kniefWarningLine = null;
+    public GameObject kniefWarningSign = null;
+    public GameObject kniefWarningLine = null;
 
     void Awake() {
         EnemyIO.getInstance.GetEnemyData(EnemyType.KniefEnemy, out enemyInfo);
         kniefSprite = this.gameObject.GetComponent<SpriteRenderer>();
         playerPos = GameObject.FindWithTag("Player").GetComponent<Transform>().position;
-        kniefWarningLine = GameObject.Find("KniefWarningLine");
-        kniefWarningSign = GameObject.Find("KniefWarningSign");
-        kniefWarningLine.SetActive(false);
-        kniefWarningSign.SetActive(false);
     }
     IEnumerator Start() {
         //밑으로 진행
         kniefSprite.enabled = false;
-        StartCoroutine(KniefWarningLine()); //경고선 처리
-        StartCoroutine(KniefWarningSign()); //경고느낌표 처리
-        yield return new WaitForSeconds(1.1f);
+        StartCoroutine(KniefWarning()); //경고선 처리, 경고느낌표 처리
+        yield return new WaitForSeconds(3.0f);
         kniefSprite.enabled = true;
         this.transform.position = new Vector2(playerPos.x, 9.0f);
-        this.transform.Translate(Vector2.down * Time.deltaTime * enemyInfo.speed); // 아래로 진행
+       
     }
     void Update() {
         this.transform.Rotate(0.0f, 0.0f, Time.deltaTime * 150); // 회전
+        this.transform.Translate(Vector2.down * Time.deltaTime * enemyInfo.speed); // 아래로 진행
     }
 
     void OnTriggerEnter2D(Collider2D coll) {
@@ -47,7 +43,7 @@ public class KniefEnemy : EnemyScript {
     //나이프경고사인 처리 코루틴
         kniefWarningSign.SetActive(true);
         kniefWarningSign.transform.position = new Vector2(playerPos.x, 9.0f);
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(1.0f);
         kniefWarningSign.SetActive(false);
     }
 }
