227146d7b56c721acb464063fe6a6657354b403d
LeahKwon <dosing87@naver.com>
2016-09-04 14:53:23 +0900

    [hee]Splash Scene

---
+++ Assets/Scripts/Intro/SplashManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashManager : MonoBehaviour {
+	public float delayTime = 3;
+
+	//Using this for initialization
+	IEnumerator Start(){
+		yield return new WaitForSeconds (delayTime);
+
+		Application.LoadLevel ("Intro");	
+	}
+}
+
